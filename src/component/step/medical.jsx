8425905bdca2a94445b45e251af7e9e47991b7e6
Black Horse <116085344+blackhorsese@users.noreply.github.com>
2023-07-13 04:22:52 +0500

    Update medical.jsx

--- src/component/step/medical.jsx
+++ src/component/step/medical.jsx
@@ -122,7 +122,7 @@ function Medical() {
             are approved and certified in Saudi Arabia
           </p>
 
-          <div className="grid grid-cols-2 lg:grid-cols-1 md:grid-cols-3 gap-5">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
             {companys.map((x) => (
               <div className="shadow-xl p-4">
                 <img src={x.img} />
