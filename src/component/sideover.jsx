f492e4ac5545f3c0df6b9241b4b015d4caade8a1
Black Horse <116085344+blackhorsese@users.noreply.github.com>
2023-08-05 20:27:28 +0500

    Update sideover.jsx

--- src/component/sideover.jsx
+++ src/component/sideover.jsx
@@ -12,7 +12,7 @@ export default function Example({ open, setOpen }) {
 
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+      <Dialog as="div" className="relative z-10 justify-between w-full" onClose={setOpen}>
         <Transition.Child
           as={Fragment}
           enter="ease-in-out duration-500"
@@ -58,27 +58,22 @@ export default function Example({ open, setOpen }) {
                       </button>
                     </div>
                   </Transition.Child>
-                  <div className="flex h-full flex-col overflow-y-scroll bg-primary py-6 shadow-xl">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-primary py-6 shadow-xl px-3">
                     <div class="container grid grid-cols-1 p-3">
                       <img className="w-1/2" src={Logo} />
-                      <p className="text-base self-center text-white mt-10">
-                        VISIT SAUDI VISA
+                      <p className="mt-10 items-center text-white">
+                        Planning a trip to Saudi Arabia? Get your visa now, it only takes 3 simple steps.
                       </p>
-
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6 self-center mt-10 text-white"
+                      <a href="/personal-info" 
+                        className="self-center uppercase mt-10 items-center text-sm font-bold text-white"
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-                        />
-                      </svg>
+                        APPLY NOW
+                      </a>
+                      <a href="/contact-us" 
+                        className="self-center uppercase mt-5 items-center text-sm font-bold text-white"
+                      >
+                        Contact Us
+                      </a>
                     </div>
                   </div>
                 </Dialog.Panel>
