991297866f1397fa269c2b1de50b8d3864f00f04
Black Horse <116085344+blackhorsese@users.noreply.github.com>
2023-10-08 02:41:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import Policy from "./pages/policy";
 import Terms from "./pages/terms";
 import Info from "./pages/info";
 import Visa from "./pages/visa";
-import PerfonalInfo from "./pages/personal";
+import Register from "./pages/register";
 import Passport from "./component/step/passport";
 import Medical from "./component/step/medical";
 import Termss from "./component/step/termss";
 import Review from "./component/step/review";
 import Method from "./component/step/method";
-import Formdata from "./pages/formdata";
 import Contact from "./component/step/contact";
 
 function App() {
@@ -22,12 +21,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/formdata" element={<Formdata />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/privact-policy" element={<Policy />} />
           <Route path="/termss" element={<Terms />} />
           <Route path="/info" element={<Info />} />
           <Route path="/visa" element={<Visa />} />
-          <Route path="/personal-info" element={<PerfonalInfo />} />
           <Route path="/passport" element={<Passport />} />
           <Route path="/medical" element={<Medical />} />
           <Route path="/terms" element={<Termss />} />
